309ee71e1dbcc4a3de0c4885dbfdc8019f5a8c39
Rob Rowe <squirrelsunite@gmail.com>
2011-01-06 19:45:43 -0600

    Created blank rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonsterManual::Application.config.secret_token = 'fb925db3ae6dc59b20d95b2c854857fd9ee2dd8f4450044f4ba220f9f7c741a29d0aee8e94a61e9ae6596f6b883d1ff03515b60346fae751ba9d313f6fe78014'
